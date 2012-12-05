964ea2e555a69d9bbb2cc57038802ce1c6d95880
Lance Pioch <lance@lancepioch.com>
2012-12-05 17:09:27 -0500

    Initial blog class added

---
+++ Blog.hx
@@ -0,0 +1,58 @@
+import sys.db.Types;
+
+
+class Blog {
+    static function main() {
+        // Open a connection
+
+        var cnx = sys.db.Mysql.connect({ 
+            host : "localhost",
+            port : 3306,
+            user : "root",
+            pass : "",
+            socket : null,
+            database : "haxe"
+        });
+
+        // Sqlite not working with C++
+        // var cnx = sys.db.Sqlite.open("haxeblog.db");
+
+        // Set as the connection for our SPOD manager
+        sys.db.Manager.cnx = cnx;
+
+        // Create the "user" table
+        if (!sys.db.TableCreate.exists(User.manager))
+            sys.db.TableCreate.create(User.manager);
+
+        var users = new Array<User>();
+
+        // Set up our test users
+        users.push(new User());
+        users.push(new User());
+
+        users[0].name = "Lance Pioch";
+        //             = new Date(year,mo,dy,h,m,s);
+        users[0].birthday = new Date(1993,02,03,0,0,0);
+        users[0].phoneNumber = "(419) 556 1337";
+        // users[0].admin = true;
+
+        users[1].name = "Derp A. Herp";
+        users[1].birthday = new Date(1990,02,21,0,0,0);
+        users[1].phoneNumber = null;
+
+        // Insert these two users into our database
+        users[0].insert();
+        users[1].insert();
+        
+        // Close the connection
+        cnx.close();
+    }
+}
+
+class User extends sys.db.Object {
+    public var id : SId;
+    public var name : SString<32>;
+    public var birthday : SDate;
+    public var phoneNumber : SNull<SText>;
+    // public var admin = false;
+}
