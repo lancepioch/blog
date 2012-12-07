860e2b261f9a1138ffbdc93a9c4d645a8290e6ab
Lance Pioch <lance@lancepioch.com>
2012-12-07 12:23:41 -0500

    connect and disconnect Database static methods to Blog

--- Blog.hx
+++ Blog.hx
@@ -1,35 +1,42 @@
 import sys.db.Types;
 
 class Blog {
+    private static var cnx : sys.db.Connection;
     public static var db = "mysql";
 
     public static function main() {
-        // Open a connection
-        var cnx : sys.db.Connection;
+        Blog.connectDatabase();
+        Blog.setupDatabase(cnx);
+        Blog.disconnectDatabase();
+    }
+
+    public static function connectDatabase() {
+        var host = "localhost";
+        var user = "root";
+        var pass = "";
+        var dbname = "haxe";
 
         if (Blog.db == "mysql")
             cnx = sys.db.Mysql.connect({ 
-                host : "localhost",
+                host : host,
                 port : 3306,
-                user : "root",
-                pass : "",
+                user : user,
+                pass : pass,
                 socket : null,
-                database : "haxe"
+                database : dbname
             });
         else if (Blog.db == "sqlite")
-            cnx = sys.db.Sqlite.open("haxe.db");
+            cnx = sys.db.Sqlite.open(dbname + ".db");
         else
             throw "Error: Must select valid database type.";
 
-        // Set as the connection for our SPOD manager
-        sys.db.Manager.cnx = cnx;
+        sys.db.Manager.cnx = Blog.cnx;
+    }
 
-        Blog.setupDatabase(cnx);
-
-
-        
-        // Close the connection
-        cnx.close();
+    public static function disconnectDatabase() {
+        if (Blog.cnx != Null)
+            Blog.cnx.close();
+        Blog.cnx = Null;
     }
 
     public static function setupDatabase(cnx : sys.db.Connection) {
@@ -56,14 +63,9 @@ class Blog {
         users.push(new User());
 
         users[0].name = "Lance Pioch";
-        //                = new Date(year,mo,dy,h,m,s);
-        users[0].birthday = new Date(1993,02,03,0,0,0);
-        users[0].phoneNumber = "(419) 556 1337";
-        // users[0].admin = true;
+        users[0].admin = true;
 
         users[1].name = "Derp A. Herp";
-        users[1].birthday = new Date(1990,02,21,0,0,0);
-        users[1].phoneNumber = null;
 
         // Insert these two users into our database
         users[0].insert();
@@ -75,7 +77,6 @@ class User extends sys.db.Object {
     public var id : SId;
     public var name : SString<32>;
     public var birthday : SDate;
-    // public var phoneNumber : SNull<SText>;
     public var admin : SBool = false;
 }
 
